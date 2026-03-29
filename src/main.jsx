218eeae421769c6cefa920f63020a1f47ca1bd40
unknown <zulfafti@fnail.com>
2026-03-29 18:08:36 +0700

    instal library

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from "react-router";
+import Zulfa from "./pages/zulfa.jsx"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/zulfa",
+    element: <Zulfa/>
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
+);
